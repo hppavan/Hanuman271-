630bce2b0ed6149ece4e56f5a2f85dbc124959a9
Salman8801 <134062350+Salman8801@users.noreply.github.com>
2023-07-15 19:49:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,9 +22,9 @@ CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 PICS = (environ.get('PICS', 'https://telegra.ph/file/7215c7831b4ae88897448.jpg https://telegra.ph/file/34f3bd6b0cda5bb9b0cca.jpg https://telegra.ph/file/724bdd44693500c024ec7.jpg https://telegra.ph/file/1b39388dd4a343cc325bc.jpg')).split()
-NOR_IMG = environ.get("NOR_IMG", "https://te.legra.ph/file/a27dc8fe434e6b846b0f8.jpg")
-MELCOW_VID = environ.get("MELCOW_VID", "https://te.legra.ph/file/6f55d902f9bf2d0afd4bb.mp4")
-SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
+NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/d4a059c7f0ffde8a45336.jpg")
+MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/d4a059c7f0ffde8a45336.jpg")
+SPELL_IMG = environ.get("https://telegra.ph/file/d4a059c7f0ffde8a45336.jpg")
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5403432874').split()]
