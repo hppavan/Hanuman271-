c7434493304074142d7503199a639f608faa6e43
Salman8801 <134062350+Salman8801@users.noreply.github.com>
2023-07-15 17:06:28 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -419,7 +419,7 @@ async def start(client, message):
             pass
         return await message.reply('No such file exist.')
     files = files_[0]
-    title = '@New_Moviee_hindi ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files.file_name.split()))
+    title = '@PR_allmovise_0 ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files.file_name.split()))
     size=get_size(files.file_size)
     f_caption=files.caption
     if CUSTOM_FILE_CAPTION:
